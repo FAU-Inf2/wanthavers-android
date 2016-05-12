6a47c3bd6756bbca1f7b0d8141888ccd0bf16d6a
Nicolas Wolf <nicolas.wolf@fau.de>
2016-05-12 10:08:13 +0200

    1st implementation of shared preferences helper

--- app/src/main/java/wanthavers/mad/cs/fau/de/wanthavers_android/rest/RestClient.java
+++ app/src/main/java/wanthavers/mad/cs/fau/de/wanthavers_android/rest/RestClient.java
@@ -5,16 +5,15 @@ import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
 
-public class RestClient {
+public abstract class RestClient {
     protected final WebTarget target;
 
-    protected final String API_URL;
+    //TODO: get API-URL from shared preferences or something like that
+    protected final String api_url = "http://faui21f.informatik.uni-erlangen.de:9090/";
 
     protected RestClient() {
-        //TODO: get API-URL from shared preferences or something like that
-        API_URL = "http://faui21f.informatik.uni-erlangen.de:9090/";
-        target = ClientBuilder.newClient().register(JacksonJsonProvider.class).target(API_URL);
+        target = ClientBuilder.newClient().register(JacksonJsonProvider.class).target(api_url);
     }
 
-    public String getApiUrl() { return API_URL; }
+    public String getApiUrl() { return api_url; }
 }
---
+++ app/src/main/java/wanthavers/mad/cs/fau/de/wanthavers_android/util/SharedPreferencesHelper.java
@@ -0,0 +1,61 @@
+package wanthavers.mad.cs.fau.de.wanthavers_android.util;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import javax.inject.Inject;
+
+public class SharedPreferencesHelper {
+    protected final String sharedPreferencesName;
+    protected final SharedPreferences sharedPreferences;
+    protected final Context context;
+    protected final SharedPreferences.Editor editor;
+
+    //Shared Preferences Names
+    public static final String NAME_SETTINGS = "settings";
+
+    //Key Stringsg
+    public static final String KEY_API_URL = "api_url";
+
+    @Inject
+    public SharedPreferencesHelper(String sharedPreferencesName, Context context) {
+        if(context == null) {
+            throw new IllegalArgumentException("Context must not be null!");
+        }
+        if(sharedPreferencesName.isEmpty() || sharedPreferencesName == null) {
+            throw new IllegalArgumentException("SharedPreferencesName must not be empty/null!");
+        }
+
+        this.sharedPreferencesName = sharedPreferencesName;
+        this.context = context;
+        this.sharedPreferences = context.getSharedPreferences(sharedPreferencesName, Context.MODE_PRIVATE);
+        this.editor = this.sharedPreferences.edit();
+    }
+
+    public void saveString(String key, String value) {
+        editor.putString(key, value);
+        editor.apply();
+    }
+
+    public String loadString(String key, String defaultValue) {
+        return sharedPreferences.getString(key, defaultValue);
+    }
+
+    public void saveInt(String key, int value) {
+        editor.putInt(key, value);
+        editor.apply();
+    }
+
+    public int loadInt(String key, int defaultValue) {
+        return sharedPreferences.getInt(key, defaultValue);
+    }
+
+    public void saveBoolean(String key, Boolean value) {
+        editor.putBoolean(key, value);
+        editor.apply();
+    }
+
+    public Boolean loadBoolean(String key, Boolean defaultValue) {
+        return sharedPreferences.getBoolean(key, defaultValue);
+    }
+}
