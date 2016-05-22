0dc23e302b6ca175f4ad8e6d095e4c48ac58de0c
Nicolas Wolf <nicolas.wolf@fau.de>
2016-05-22 11:10:44 +0200

    added media repository

---
+++ app/src/main/java/wanthavers/mad/cs/fau/de/wanthavers_android/rest/MediaClient.java
@@ -0,0 +1,45 @@
+package wanthavers.mad.cs.fau.de.wanthavers_android.rest;
+
+import android.content.Context;
+
+import org.glassfish.jersey.client.proxy.WebResourceFactory;
+import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
+
+import java.io.InputStream;
+import java.util.List;
+
+import de.fau.cs.mad.wanthavers.common.Media;
+import de.fau.cs.mad.wanthavers.common.rest.api.MediaResource;
+
+/**
+ * Created by Nico on 22.05.2016.
+ */
+public class MediaClient extends RestClient {
+    private static MediaClient INSTANCE;
+
+    private MediaResource mediaEndpoint;
+
+    private MediaClient(Context context) {
+        super(context);
+        mediaEndpoint = WebResourceFactory.newResource(MediaResource.class, target);
+    }
+
+    public static MediaClient getInstance(Context context) {
+        if(INSTANCE == null) {
+            INSTANCE = new MediaClient(context);
+        }
+        return INSTANCE;
+    }
+
+    public List<Media> getAllMedia() {
+        return mediaEndpoint.getAll();
+    }
+
+    public Media get(long mediaId) {
+        return mediaEndpoint.get(mediaId);
+    }
+
+    public Media createMedia(InputStream inputStream, FormDataContentDisposition contentDispositionHeader) {
+        return mediaEndpoint.createMedia(inputStream, contentDispositionHeader);
+    }
+}
