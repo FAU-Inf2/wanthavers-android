4e43e105adae27529551a0b9b30b718252b1fc52
Nicolas Wolf <nicolas.wolf@fau.de>
2016-06-05 22:38:49 +0200

    added email to shared preferences

--- app/src/main/java/wanthavers/mad/cs/fau/de/wanthavers_android/rest/RestClient.java
+++ app/src/main/java/wanthavers/mad/cs/fau/de/wanthavers_android/rest/RestClient.java
@@ -38,10 +38,10 @@ public abstract class RestClient {
     protected void buildNew() {
         target = null;
         SharedPreferencesHelper sharedPreferences = SharedPreferencesHelper.getInstance(SharedPreferencesHelper.NAME_USER, context);
-        long userId = sharedPreferences.loadLong(SharedPreferencesHelper.KEY_USERID, -1337L);
+        String email = sharedPreferences.loadString(SharedPreferencesHelper.KEY_USER_EMAIL, "");
         String password = sharedPreferences.loadString(SharedPreferencesHelper.KEY_PASSWORD, "");
 
-        HttpAuthenticationFeature basicAuthFeature = HttpAuthenticationFeature.basic(String.valueOf(userId), password);
+        HttpAuthenticationFeature basicAuthFeature = HttpAuthenticationFeature.basic(email, password);
         target = ClientBuilder.newClient().register(JacksonJsonProvider.class).register(basicAuthFeature).target(API_URL);
         buildNewEndpoint();
     }
