6815ea6dd93558da74652ddb9e3714f54bdd01fa
Oliver Lutz <ollilutz@web.de>
2016-06-15 00:10:53 +0200

    remove doubles from strings

--- app/src/main/java/wanthavers/mad/cs/fau/de/wanthavers_android/filtersetting/FilterSettingFragment.java
+++ app/src/main/java/wanthavers/mad/cs/fau/de/wanthavers_android/filtersetting/FilterSettingFragment.java
@@ -7,6 +7,8 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
@@ -84,31 +86,6 @@ public class FilterSettingFragment extends Fragment implements FilterSettingCont
                 mListAdapter.setSelected(selected);
             }
         });*/
-
-        /*Button button = mFilterSettingFragBinding.buttonSmallPrice;
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mFilterSettingActionHandler.buttonChangeColor(1);
-            }
-        });
-
-        button = mFilterSettingFragBinding.buttonMediumPrice;
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mFilterSettingActionHandler.buttonChangeColor(2);
-            }
-        });
-
-        button = mFilterSettingFragBinding.buttonLargePrice;
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mFilterSettingActionHandler.buttonChangeColor(3);
-            }
-        });*/
-
         autoCompleteTextView.setAdapter(mListAdapter);
 
         //Set up radius spinner
