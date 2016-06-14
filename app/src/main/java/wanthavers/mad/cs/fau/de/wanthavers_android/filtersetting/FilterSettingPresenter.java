d48ae939c643556f7b6b4b2f006a6342c08f14d6
Oliver Lutz <ollilutz@web.de>
2016-06-14 23:48:04 +0200

    Fix some AutoCompleteView stuff, added values to FilterObject, integrate Map and Buttoninteraction

--- app/src/main/java/wanthavers/mad/cs/fau/de/wanthavers_android/filtersetting/FilterSettingPresenter.java
+++ app/src/main/java/wanthavers/mad/cs/fau/de/wanthavers_android/filtersetting/FilterSettingPresenter.java
@@ -1,15 +1,21 @@
 package wanthavers.mad.cs.fau.de.wanthavers_android.filtersetting;
 
 import android.support.annotation.NonNull;
+import android.widget.AutoCompleteTextView;
 import android.widget.EditText;
 import android.widget.RatingBar;
+import android.widget.Spinner;
 
+import com.j256.ormlite.stmt.query.In;
+
+import java.lang.reflect.Array;
 import java.util.List;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
 import de.fau.cs.mad.wanthavers.common.Category;
 import de.fau.cs.mad.wanthavers.common.DesireFilter;
+import de.fau.cs.mad.wanthavers.common.Location;
 import wanthavers.mad.cs.fau.de.wanthavers_android.R;
 import wanthavers.mad.cs.fau.de.wanthavers_android.baseclasses.UseCase;
 import wanthavers.mad.cs.fau.de.wanthavers_android.baseclasses.UseCaseHandler;
@@ -72,19 +78,81 @@ public class FilterSettingPresenter implements FilterSettingContract.Presenter {
         DesireFilter desireFilter = new DesireFilter();
 
         //get all Views
-        RatingBar minRatingBar = (RatingBar)mActivity.findViewById(R.id.filter_setting_RatingBar);
+        InstantAutoComplete autoComplete = (InstantAutoComplete) mActivity.findViewById(R.id.spinner_category);
+        RatingBar minRatingBar = (RatingBar)mActivity.findViewById(R.id.filterSettingRatingBar);
         EditText minRewardView = (EditText) mActivity.findViewById(R.id.filter_min_reward);
+        Spinner radiusView = (Spinner) mActivity.findViewById(R.id.spinner_radius);
 
-        //get all Values
+        //Category
+        /*Category selectedCategory = autoComplete.
+        if (selectedCategory != null) {
+            desireFilter.setCategory(selectedCategory.getId());
+            System.out.println("Found Category");
+        }*/
+        /*int pos = autoComplete.getListSelection();
+        Category category = (Category) autoComplete.getAdapter().getItem(pos);
+        System.out.println(category.getName());*/
+
+        //Price
+        int priceClicked = mFilterSettingView.getPriceClicked();
+        //TODO: max price not hard coded!!!
+        switch (priceClicked) {
+            case 1:
+                desireFilter.setPrice_max(10.0);
+                break;
+            case 2:
+                desireFilter.setPrice_max(50.0);
+                break;
+            case 3:
+                desireFilter.setPrice_max(100.0);
+                break;
+            default:
+                break;
+        }
+        System.out.println(desireFilter.getPrice_max());
+
+        //Min_Reward
+        if (!minRewardView.getText().toString().equals("")) {
+            Double minReward = Double.valueOf(minRewardView.getText().toString());
+            desireFilter.setReward_min(minReward);
+        }
+        System.out.println(desireFilter.getReward_min());
+
+        //Min_Rating
         float minRating = minRatingBar.getRating();
-        Double minReward = Double.valueOf(minRewardView.getText().toString());
-
-        //set all filter values
-
         desireFilter.setRating_min(minRating);
-        desireFilter.setReward_min(minReward);
+        System.out.println(desireFilter.getRating_min());
+
+        //Location
+        //TODO: make local list with names
+        Location location = mFilterSettingView.getLocation();
+        if (location == null) {
+            return;
+        }
+        desireFilter.setLon(location.getLon());
+        desireFilter.setLat(location.getLat());
+
+        //Radius
+        //TODO: differ between miles and kilometres
+        //now only km
+        String radius = (String) radiusView.getSelectedItem();
+        String[] array = mFilterSettingView.getRadiusArray();
+        if (radius.equals(array[0])) {
+            desireFilter.setRadius(1.0);
+        } else if (radius.equals(array[1])) {
+            desireFilter.setRadius(2.0);
+        } else if (radius.equals(array[2])) {
+            desireFilter.setRadius(5.0);
+        } else if (radius.equals(array[3])) {
+            desireFilter.setRadius(10.0);
+        } else {
+            //no restriction
+        }
+        System.out.println(desireFilter.getRadius());
 
         setFilter(desireFilter);
+
+        mFilterSettingView.showFilterChangeSuccess();
 
         mFilterSettingView.showDesireList();
     }
@@ -92,5 +160,10 @@ public class FilterSettingPresenter implements FilterSettingContract.Presenter {
     @Override
     public void setFilter(DesireFilter desireFilter){
         WantHaversApplication.setCurDesireFilter(desireFilter);
+    }
+
+    @Override
+    public void openMap() {
+        mFilterSettingView.showMap();
     }
 }
--- app/src/main/java/wanthavers/mad/cs/fau/de/wanthavers_android/filtersetting/InstantAutoComplete.java
+++ app/src/main/java/wanthavers/mad/cs/fau/de/wanthavers_android/filtersetting/InstantAutoComplete.java
@@ -4,7 +4,10 @@ import android.annotation.TargetApi;
 import android.content.Context;
 import android.graphics.Rect;
 import android.os.Build;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.AutoCompleteTextView;
 
