3b3b888d79e9cd2392bb7d8738e531402f4538c8
Julian Gruendner <julian.gruendner@shoptimax.de>
2016-06-18 18:18:22 +0200

    changing the look and feel of the app

---
+++ app/src/main/java/wanthavers/mad/cs/fau/de/wanthavers_android/util/WantHaversTextView.java
@@ -0,0 +1,15 @@
+package wanthavers.mad.cs.fau.de.wanthavers_android.util;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+public class WantHaversTextView extends TextView {
+
+
+    public WantHaversTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        this.setTypeface(Typeface.createFromAsset(context.getAssets(),"fonts/Mallanna-Regular.ttf"));
+    }
+}
