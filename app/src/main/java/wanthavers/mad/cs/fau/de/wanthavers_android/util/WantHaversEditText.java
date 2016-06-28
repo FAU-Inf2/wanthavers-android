d0521b41571d4ed30d41512f1a1dc24fb13eea7d
Oliver Lutz <ollilutz@web.de>
2016-06-28 22:09:00 +0200

    Fix layouts

---
+++ app/src/main/java/wanthavers/mad/cs/fau/de/wanthavers_android/util/WantHaversEditText.java
@@ -0,0 +1,14 @@
+package wanthavers.mad.cs.fau.de.wanthavers_android.util;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+import android.widget.EditText;
+
+public class WantHaversEditText extends EditText{
+
+    public WantHaversEditText(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        this.setTypeface(Typeface.createFromAsset(context.getAssets(),"fonts/Mallanna-Regular.ttf"));
+    }
+}
