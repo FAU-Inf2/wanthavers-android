a13c21dc2ade91eb8cf20e8ba3ddc910b926fb00
Oliver Lutz <ollilutz@web.de>
2016-10-05 21:16:45 +0200

    Desire Detail: Swipe down to refresh view

--- app/src/main/java/wanthavers/mad/cs/fau/de/wanthavers_android/desiredetail/DesireDetailFragment.java
+++ app/src/main/java/wanthavers/mad/cs/fau/de/wanthavers_android/desiredetail/DesireDetailFragment.java
@@ -8,6 +8,8 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -93,6 +95,7 @@ public class DesireDetailFragment extends Fragment implements DesireDetailContra
     @Override
     public void onResume() {
         super.onResume();
+        mDesireDetailFragBinding.desireDetailSwipeView.setRefreshing(true);
         mPresenter.start();
     }
 
@@ -126,9 +129,24 @@ public class DesireDetailFragment extends Fragment implements DesireDetailContra
         mListAdapter = new DesireDetailAdapter(new ArrayList<Haver>(0), mPresenter, mDesireDetailActionHandler, mDesireLogic);
         mRecyclerView.setAdapter(mListAdapter);
 
-        //ScrollView
+        //Set up ScrollView
         ScrollView scrollView = mDesireDetailFragBinding.desireDetailMainScreen;
         scrollView.setOnTouchListener(new OnSwipeTouchListener(getActivity(), mPresenter));
+
+        //Set up SwipeRefreshLayout
+        SwipeRefreshLayout swipeRefreshLayout = mDesireDetailFragBinding.desireDetailSwipeView;
+        swipeRefreshLayout.setColorSchemeColors(
+                ContextCompat.getColor(getActivity(), R.color.colorPrimary),
+                ContextCompat.getColor(getActivity(), R.color.colorAccent),
+                ContextCompat.getColor(getActivity(), R.color.colorPrimaryDark)
+        );
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                mPresenter.loadDesire();
+            }
+        });
+        swipeRefreshLayout.setRefreshing(true);
 
         return mDesireDetailFragBinding.getRoot();
     }
@@ -286,12 +304,15 @@ public class DesireDetailFragment extends Fragment implements DesireDetailContra
             showWanter();
 
             mDesireDetailFragBinding.cancelDesire.setVisibility(View.VISIBLE);
+            mDesireDetailFragBinding.acceptDesire.setVisibility(View.GONE);
 
             if(isAdded()) {
                 showStatus(getString(R.string.haver_status_accepted));
             }
         } else {
             showWanter();
+            mDesireDetailFragBinding.acceptDesire.setVisibility(View.GONE);
+            mDesireDetailFragBinding.cancelDesire.setVisibility(View.GONE);
             if(isAdded()) {
                 showStatus(getString(R.string.haver_status_rejected));
             }
@@ -320,11 +341,15 @@ public class DesireDetailFragment extends Fragment implements DesireDetailContra
             hideFinishDesire();
         } else if (isAcceptedHaver){
             showWanter();
+            mDesireDetailFragBinding.acceptDesire.setVisibility(View.GONE);
+            mDesireDetailFragBinding.cancelDesire.setVisibility(View.GONE);
         } else {
             showWanter();
             if(isAdded()) {
                 showStatus(getString(R.string.haver_status_rejected));
             }
+            mDesireDetailFragBinding.acceptDesire.setVisibility(View.GONE);
+            mDesireDetailFragBinding.cancelDesire.setVisibility(View.GONE);
         }
         endLoadingProgress();
     }
@@ -431,6 +456,8 @@ public class DesireDetailFragment extends Fragment implements DesireDetailContra
         mDesireDetailFragBinding.desireDetailMainScreen.setVisibility(View.VISIBLE);
         mLoadFinishedFlag = true;
         onPrepareOptionsMenu(mOptionsMenu);
+
+        mDesireDetailFragBinding.desireDetailSwipeView.setRefreshing(false);
     }
 
     private void hideFinishDesire() {
